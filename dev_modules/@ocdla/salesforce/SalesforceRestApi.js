f5a625d9f586319242d3dbd912b448a588ecb1b2
Charlese Muchmore <charlymuchmore@gmail.com>
2024-04-24 13:54:26 -0700

    Implemented some JSDOC comments

--- dev_modules/@ocdla/salesforce/SalesforceRestApi.js
+++ dev_modules/@ocdla/salesforce/SalesforceRestApi.js
@@ -1,27 +1,57 @@
-class SalesforceRestApi {
 
+/**
+ * @module SalesforceRestApi
+ * @classdesc This module is to be used to interface with the Salesforce API using a RESTful architecture. 
+ */
+class SalesforceRestApi {
     instanceUrl;
     accessToken;
     method;
     body;
+    /**
+     * Represents the Salesforce API version being used.
+     * @static
+     * */
     static API_VERSION = 'v60.0';
+    /**
+     * Represents the base Salesforce API endpoint used for http requests.
+     * @static
+     * */
     static BASE_URL = '/services/data/' + SalesforceRestApi.API_VERSION + '/';
 
+    /** 
+     * Represents this API that interfaces with the Salesforce API.
+     * @constructor 
+     * @param {string} instanceUrl - The
+     * @param {string} accessToken - The
+    */
     constructor(instanceUrl, accessToken) {
         this.instanceUrl = instanceUrl;
         this.accessToken = accessToken;
+        /**@property */
         this.headers = new Headers();
         this.authHeader = "Bearer " + this.accessToken; 
         this.headers.append("Authorization", this.authHeader);
         this.headers.append('Content-Type', 'application/json');
     }
 
+    /** 
+     * @param {string} queryString - The SQL query.
+     * @returns {object}
+     * @example
+     * "SELECT Name,Id FROM Object_Name"
+    */
     query(queryString) {
         this.method = 'GET';
         this.path = SalesforceRestApi.BASE_URL + 'query?q=' + queryString;
         return this.send();
     }
 
+    /** 
+     * @param {string} objectName
+     * @param {object} record
+     * @returns {object}
+    */
     create(objectName, record) {
         this.method = 'POST';
         this.path = SalesforceRestApi.BASE_URL + 'sobjects/' + objectName; 
@@ -30,6 +60,11 @@ class SalesforceRestApi {
         return this.send();
     }
 
+    /** 
+     * @param {string} objectName
+     * @param {object} record
+     * @returns {object}
+    */
     update(objectName, record) {
         this.method = 'PATCH';
         this.path = SalesforceRestApi.BASE_URL + 'sobjects/' + objectName + `/${record.id}`;
@@ -38,6 +73,11 @@ class SalesforceRestApi {
         return this.send();
     }
 
+    /** 
+     * @param {string} objectName
+     * @param {object} record
+     * @returns {object}
+    */
     delete(objectName, record) {
         this.method = 'DELETE';
         this.path = SalesforceRestApi.BASE_URL + 'sobjects/' + objectName + `/${record.id}`;
@@ -45,6 +85,9 @@ class SalesforceRestApi {
         return this.send();
     }
 
+    /** 
+     * @returns {object}
+    */
     send() {
         let config = {
             method: this.method,
